ff38695e33acb43e0e31dd0eaa48ba564a4e8e2b
Anton Lenev <m0rg0t.Anton@gmail.com>
2013-11-06 13:17:02 +0400

    Updated library; removed fancybox from wp plugin
    Updated library; removed fancybox from wordpress plugin

--- wordpress-plugin/probtn/start_probtn.php
+++ wordpress-plugin/probtn/start_probtn.php
@@ -2,7 +2,7 @@ jQuery(document).ready(function() {
 	jQuery(document).StartButton({
 		"mainStyleCss": "<?php echo $_GET["mainStyleCss"]; ?>",
 		"jqueryPepPath": "<?php echo $_GET["jqueryPepPath"]; ?>",
-		"fancyboxJsPath": "<?php echo $_GET["fancyboxJsPath"]; ?>",
-		"fancyboxCssPath": "<?php echo $_GET["fancyboxCssPath"]; ?>",
+		//"fancyboxJsPath": "<?php echo $_GET["fancyboxJsPath"]; ?>",
+		//"fancyboxCssPath": "<?php echo $_GET["fancyboxCssPath"]; ?>",
 	});
 });
